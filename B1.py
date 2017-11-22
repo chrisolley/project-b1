672be7120edc69a402c83a3d038b1c509a775114
Chris Olley <olley.cj@gmail.com>
2017-11-22 14:00:22 +0000

    minor changes

--- B1.py
+++ B1.py
@@ -1,88 +1,111 @@
 # -*- coding: utf-8 -*-
-import numpy as np 
+# !python3
+import numpy as np
 import matplotlib.pyplot as plt
 import math
 from tqdm import tqdm
 from minimizer import minimise
 
-def fit(tau, t, s): 
 
-	'''
-	fit: Theoretical distribution for the measurements of decay lifetimes of subatomic particles
-	Args: 
-		t: measured lifetime (independent variable)
-		tau: average lifetime
-		s: measurement error
-	'''
+def fit(tau, t, s):
+    '''
+    fit: Theoretical distribution for the measurements of decay lifetimes
+    of subatomic particles. Convolution of an exponential distribution
+    and a gaussian.
+    Args:
+        t: measured lifetime (independent variable).
+        tau: average lifetime.
+        s: measurement error.
+    Returns:
+        f: value of lifetime distribution at a point t
+        for a given mean lifetime and measurement error.
+    '''
 
-	return (1./(2.*tau))*np.exp((s**2/(2*tau**2))-t/tau)*math.erfc((1./np.sqrt(2))*(s/tau-t/s))
+    f = (1. / (2. * tau)) * np.exp((s**2 / (2 * tau**2)) - t / tau) * \
+        math.erfc((1. / np.sqrt(2)) * (s / tau - t / s))
+
+    return f
 
 
 def nll(tau, lifetime, uncertainty):
-	'''
-	nll: Negative Log Likelihood, calculates the likelihood of an ensemble of PDFs.
-	Args:
-		tau: average lifetime.
-		lifetime: list of lifetime measurements.
-		uncertainty: list of uncertainties associated with lifetime measurements.
-	Returns: 
-		nll: negative log likelihood.
-	'''
-	prob_list = []
+    '''
+    nll: Negative Log Likelihood, calculates the likelihood of an ensemble
+    of PDFs.
+    Args:
+        tau: average lifetime.
+        lifetime: list of lifetime measurements.
+        uncertainty: list of uncertainties associated with lifetime
+        measurements.
+    Returns:
+        nll: negative log likelihood.
+    '''
 
-	for (t,s) in zip(lifetime, uncertainty):
-		prob = fit(tau,t,s)
-		prob_list.append(np.log(prob))
+    prob_list = []
 
-	nll = -sum(prob_list)
+    for (t, s) in zip(lifetime, uncertainty):
+        prob = fit(tau, t, s)
+        prob_list.append(np.log(prob))
 
-	return nll
+    nll = -sum(prob_list)
 
-#Filling arrays & reading data
+    return nll
 
-f = open('lifetime.txt') # measured lifetimes and uncertainty data
+# Filling arrays & reading data
+
+
+f = open('lifetime.txt')  # measured lifetimes and uncertainty data
 lifetime = []
 uncertainty = []
 
-for line in f: # reading data
-	data = line.split()
-	lifetime.append(float(data[0]))
-	uncertainty.append(float(data[1]))
+for line in f:  # reading data
+    data = line.split()
+    lifetime.append(float(data[0]))
+    uncertainty.append(float(data[1]))
 
-N = len(lifetime) # number of data points
+N = len(lifetime)  # number of data points
 
-
-t = np.linspace(0.,10.,1000)
+t = np.linspace(0., 10., 1000)
 g = []
-tau = np.linspace(0.,5.,100)
+tau = np.linspace(0., 5., 100)
 nll_list = []
 
-for a in tqdm(t, total=len(t), desc='Fit function array filling...'): 
-        g.append(fit(tau=1.,t=a,s=.05))
+for a in tqdm(t, total=len(t), desc='Fit function array filling...'):
+    g.append(fit(tau=1., t=a, s=.05))
 
-for a in tqdm(tau, total=len(tau), desc='NLL array filling...'): 
-	nll_list.append(nll(tau=a, lifetime=lifetime, uncertainty=uncertainty))
+for a in tqdm(tau, total=len(tau), desc='NLL array filling...'):
+    nll_list.append(nll(tau=a, lifetime=lifetime, uncertainty=uncertainty))
 
 
-points_hist = minimise(nll, (0.1, 0.3, 1.0), 10**(-5), lifetime, uncertainty)
-#TODO what starting values does this work for, how to treat negative values that appear from the minimisation algorithm.
-print points_hist[-1]
+points_hist, points_initial = minimise(nll, (0.1, 2.0, 1.0), 10**(-5),
+                                       lifetime, uncertainty)
 
-#Plotting
+# e.g. (0.1, 2.0, 1.0) doesn't give min, (0.1, 0.3, 1.5) goes to
+# negative x values, (0.1, 0.3, 1.0) works.
+# TODO what starting values does this work for,
+# how to treat negative values that appear from the minimisation algorithm.
+# ANSWER need to select points that are along an interval of positive curvature. 
+# could use an interpolation algorithm first to determine roughly where the minimum is.
 
-fig1,ax1 = plt.subplots()
-ax1.hist(lifetime, bins = int(np.sqrt(N)))
-ax1.grid()
-ax1.set_xlim(0,7)
+# Plotting
 
-fig2,ax2 = plt.subplots()
-ax2.plot(t,g)
-ax2.set_xlim(0,7)
-ax2.grid()
+# fig1, ax1 = plt.subplots()
+# ax1.hist(lifetime, bins=int(np.sqrt(N)))
+# ax1.set_xlim(0, 7)
+# ax1.grid()
 
-fig3,ax3 = plt.subplots()
+# fig2, ax2 = plt.subplots()
+# ax2.plot(t, g)
+# ax2.set_xlim(0, 7)
+# ax2.grid()
+
+fig3, ax3 = plt.subplots()
 ax3.plot(tau, nll_list)
-ax3.plot([elem[0] for elem in points_hist], [elem[1] for elem in points_hist], color='red', linestyle='', marker='.')
+ax3.plot([elem[0] for elem in points_hist],
+         [elem[1] for elem in points_hist],
+         color='red', linestyle='', marker='.')
+ax3.plot([elem[0] for elem in points_initial],
+         [elem[1] for elem in points_initial],
+         color='green', linestyle='', marker='.')
 ax3.grid()
 
 plt.show()
--- minimizer.py
+++ minimizer.py
@@ -1,68 +1,88 @@
+# -*- coding: utf-8 -*-
+# !python3
 import matplotlib.pyplot as plt
 import numpy as np
-from tqdm import tqdm 
-
-def para_min(f,x, *args): 
-	'''
-	para_min: Computes the minimum of the parabola formed by three points.
-	Args: 
-		f: function giving y coordinate of points.
-		x: list of three points. 
-		*args: function arguments.
-
-	Returns: 
-		x_min: x coord of minimum.
-	'''
-
-	num = 0.5*((x[2]**2-x[1]**2)*f(x[0],*args)+(x[0]**2-x[2]**2)*f(x[1],*args)+(x[1]**2-x[0]**2)*f(x[2],*args))
-	denom = ((x[2]-x[1])*f(x[0],*args)+(x[0]-x[2])*f(x[1],*args)+(x[1]-x[0])*f(x[2],*args))
-	x_min = num/denom
-	return x_min
+from tqdm import tqdm
 
 
-def minimise(f, start_points, epsilon, *args): 
-	'''
-	minimise: Minimises a function using parabolic minimisation algorithm.
-	Args: 
-		f: function to be minimised. 
-		start_points: list of three initial points ,must be different. 
-		epsilon: required precision.
-		*args: function arguments.
-	'''
-	# print f(start_points, *args)
-	points = zip(start_points, [f(p, *args) for p in start_points])
-	points_hist = []
+def para_min(f, x, *args):
+    '''
+    para_min: Computes the minimum of the parabola formed by three points.
+    Args:
+        f: function giving y coordinate of points.
+        x: list of three points.
+        *args: function arguments.
 
-	for i in range(2):
-		x_update = para_min(f, [elem[0] for elem in points], *args)
-		point_update = (x_update, f(x_update, *args)) 
-		points.append(point_update)
-		points_hist.append(point_update)
-		points.sort(key=lambda tup: tup[1])
-		points.pop()
+    Returns:
+        x_min: x coord of minimum.
+    '''
 
-	conv = abs((points_hist[-1][0]-points_hist[-2][0])/points_hist[-2][0])
-	i=1
-	while conv>epsilon:
-		print 'Parabolic Minimisation loop {}'.format(i)
-		x_update = para_min(f, [elem[0] for elem in points], *args)
-		point_update = (x_update, f(x_update, *args)) 
-		points.append(point_update)
-		points_hist.append(point_update)
-		points.sort(key=lambda tup: tup[1])
-		points.pop()
-		conv = abs((points_hist[-1][0]-points_hist[-2][0])/points_hist[-2][0])
-		i+=1
-	return points_hist
+    num = 0.5 * ((x[2]**2 - x[1]**2) * f(x[0], *args) +
+                 (x[0]**2 - x[2]**2) * f(x[1], *args) +
+                 (x[1]**2 - x[0]**2) * f(x[2], *args))
+    denom = ((x[2] - x[1]) * f(x[0], *args) +
+             (x[0] - x[2]) * f(x[1], *args) +
+             (x[1] - x[0]) * f(x[2], *args))
+    x_min = num / denom
+    return x_min
+
+
+def minimise(f, start_points, epsilon, *args):
+    '''
+    minimise: Minimises a function using parabolic minimisation algorithm.
+    Args:
+        f: function to be minimised.
+        start_points: list of three initial points ,must be different.
+        epsilon: required precision.
+        *args: function arguments.
+    '''
+    # print f(start_points, *args)
+    # creates a tuple of points based on the starting x values
+    points_initial = zip(start_points, [f(p, *args) for p in start_points])
+    points = zip(start_points, [f(p, *args) for p in start_points])
+    points_hist = []  # initialise array to hold past points
+
+    for i in range(2):  # runs the algorithm twice
+        x_update = para_min(f, [elem[0] for elem in points], *args)
+        point_update = (x_update, f(x_update, *args))
+        points.append(point_update)
+        points_hist.append(point_update)
+        print points
+        points.sort(key=lambda tup: tup[1])
+        print points
+        points.pop()
+
+    conv = abs((points_hist[-1][0] -
+                points_hist[-2][0]) / points_hist[-2][0])
+
+    i = 1
+    while (conv > epsilon):
+        print 'Parabolic minimisation loop {}'.format(i)
+        x_update = para_min(f, [elem[0] for elem in points], *args)
+        point_update = (x_update, f(x_update, *args))
+        points.append(point_update)
+        points_hist.append(point_update)
+        print points
+        points.sort(key=lambda tup: tup[1])
+        print points
+        points.pop()
+        conv = abs((points_hist[-1][0] -
+                    points_hist[-2][0]) / points_hist[-2][0])
+        i += 1
+
+    return points_hist, points_initial
+
 
 def f(x, a, b):
-	return a*np.cosh(x)+b
+
+    return a * np.cosh(x) + b
 
 # points_hist = minimise(f,(-5, 7, 10),0.01,2,2)
 # print [elem[0] for elem in points_hist]
 # print [elem[1] for elem in points_hist]
 # x = np.linspace(-5,5,100)
 # plt.plot(x, f(x, 2, 2), color='blue', linestyle='-')
-# plt.plot([elem[0] for elem in points_hist], [elem[1] for elem in points_hist], color='red', linestyle='', marker='.')
+# plt.plot([elem[0] for elem in points_hist], 
+# [elem[1] for elem in points_hist], color='red', linestyle='', marker='.')
 # plt.grid()
 # plt.show()
