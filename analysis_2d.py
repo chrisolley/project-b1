fixed gradient method

analysis_2d.py
@@ -2,9 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
 from tqdm import tqdm
 from b1 import nll_2d
-from min_2d import grad_min, newton_min
+from min_2d import grad_min, newton_min, grad_cds, grad_fds
 
 f = open('lifetime.txt')  # measured lifetimes and uncertainty data
 lifetime = []
@@ -18,59 +19,92 @@ for line in f:  # reading data line by line
 N = len(lifetime)  # number of data points
 
 #start_point = (.4, .6) #(0.4, 0.6) works
-start_point = (.41, .98)
-sol1 = grad_min(nll_2d, start_point, 0.0001, lifetime, uncertainty)
-#sol2 = newton_min(nll_2d, start_point, lifetime, uncertainty)
-#x_hist1 = sol1[0]
-print(sol1[0])
-#x_hist2 = sol2[0]
-#f_hist2 = sol2[1]
+start_point = (.46, 0.95)
+sol1 = grad_min(nll_2d, start_point, 10**(-5), lifetime, uncertainty)
+sol2 = newton_min(nll_2d, start_point, lifetime, uncertainty)
+x_hist1 = sol1[0]
+f_hist1 = sol1[1]
+
+#print(sol1[0])
+x_hist2 = sol2[0]
+f_hist2 = sol2[1]
 #print(sol2[0])
 #
-#grid_points = 20
-#tau = np.linspace(0.2, 1., grid_points) # range for nll function
-#a = np.linspace(0.6, 1., grid_points)
-#
-#nll_matrix = np.zeros((grid_points, grid_points)) # initialise array for nll function plotting
-#
-#for i, t in enumerate(tqdm(tau, desc="Tau loop")):# tau loop {}'.format(i)):
-#    for j, b in enumerate(tqdm(a, desc="a loop", leave=False)):# a loop {}'.format(j)):
-#    #tqdm(tau, total=len(tau), desc='NLL array filling...'):
-#        nll_matrix[j, i] = nll_2d(tau=t, a=b, lifetime=lifetime, uncertainty=uncertainty)
-#
-##Plotting
-#fig1, ax1 = plt.subplots()
+grid_points = 10
+tau = np.linspace(0.38, 0.46, grid_points) # range for nll function
+a = np.linspace(0.94, 1., grid_points)
+
+nll_matrix = np.zeros((grid_points, grid_points)) # initialise array for nll function plotting
+#gradient_matrix_x = np.zeros((grid_points, grid_points))
+#gradient_matrix_y = np.zeros((grid_points, grid_points))
+
+for i, t in enumerate(tqdm(tau, desc="Tau loop")):# tau loop {}'.format(i)):
+    for j, b in enumerate(tqdm(a, desc="a loop", leave=False)):# a loop {}'.format(j)):
+    #tqdm(tau, total=len(tau), desc='NLL array filling...'):
+        nll_matrix[j, i] = nll_2d(tau=t, a=b, lifetime=lifetime, uncertainty=uncertainty)
+        #x = np.asarray((t,b)).reshape((2,1))
+        #gradient_matrix_x[j, i] = grad_cds(nll_2d, x, lifetime, uncertainty)[0,0]
+        #gradient_matrix_y[j, i] = grad_cds(nll_2d, x, lifetime, uncertainty)[1,0]
+        
+#gradient_np = np.gradient(nll_matrix)
 #T, A = np.meshgrid(tau,a)
 #
-#nll_2d_min, nll_2d_max = nll_matrix.min(), nll_matrix.max()
-#
-#contours = ax1.contour(T, A, nll_matrix, 10, colours='black')
-#plt.clabel(contours, inline=True)
-#
-#v = np.linspace(nll_2d_min, nll_2d_max, 10, endpoint=True)
-#contourf = ax1.contourf(T, A, nll_matrix, levels=v, cmap=cm.viridis, alpha=0.5)
-##contour = ax1.contour(T, A, nll_matrix)
-##ax1.plot([a[0] for a in x_hist1], [a[1] for a in x_hist1], color='green', 
-##         linestyle='--', marker='.', label='Gradient Method')
-##ax1.imshow(nll_matrix, extent=[tau.min(),tau.max(), a.min(), a.max()],
-##           cmap='RdGy', alpha=.5)
-#
-#ax1.plot([a[0] for a in x_hist2], [a[1] for a in x_hist2], color='red', 
-#         linestyle='--', marker='.', label='Newton\'s Method')
-#
-#fig1.colorbar(contourf)
-#ax1.grid()
-#ax1.set_ylabel("a")
-#ax1.set_xlabel("tau")
-#ax1.legend(loc='best')
+#fig1, ax1 = plt.subplots()
+#contourf1 = ax1.contourf(T, A, gradient_np[1], cmap=cm.coolwarm)
+#fig1.colorbar(contourf1)
 #
 #fig2, ax2 = plt.subplots()
-##ax2.plot(range(len(x_hist1)), [a[0] for a in x_hist1], color='green', linestyle='-', marker='.', label='Gradient Method')
-#ax2.plot(range(len(x_hist2)), [a for a in f_hist2], color='red', linestyle='-', marker='.', label='Newton\'s Method')
-#ax2.grid()
-#ax2.legend(loc='best')
+#contourf2 = ax2.contourf(T, A, gradient_np[0], cmap=cm.coolwarm)
+#fig2.colorbar(contourf2)
 #
+#fig3, ax3 = plt.subplots()
+#contourf3 = ax3.contourf(T, A, gradient_matrix_x, cmap=cm.coolwarm)
+#fig3.colorbar(contourf3)
+#
+#fig3, ax3 = plt.subplots()
+#contourf3 = ax3.contourf(T, A, gradient_matrix_y, cmap=cm.coolwarm)
+#fig3.colorbar(contourf3)
 #plt.show()
-#
-##print('Gradient Method number of steps: {}'.format(len(x_hist1)))
-#print('Newton\'s Method number of steps: {}'.format(len(x_hist2)))
+##Plotting
+fig1, ax1 = plt.subplots()
+T, A = np.meshgrid(tau,a)
+
+nll_2d_min, nll_2d_max = nll_matrix.min(), nll_matrix.max()
+
+contours = ax1.contour(T, A, nll_matrix, 10, colours='black')
+plt.clabel(contours, inline=True)
+
+v = np.linspace(nll_2d_min, nll_2d_max, 10, endpoint=True)
+contourf = ax1.contourf(T, A, nll_matrix, levels=v, cmap=cm.viridis, alpha=0.5)
+#contour = ax1.contour(T, A, nll_matrix)
+ax1.plot([a[0] for a in x_hist1], [a[1] for a in x_hist1], color='green', 
+         linestyle='--', marker='.', label='Gradient Method')
+#ax1.imshow(nll_matrix, extent=[tau.min(),tau.max(), a.min(), a.max()],
+#           cmap='RdGy', alpha=.5)
+
+ax1.plot([a[0] for a in x_hist2], [a[1] for a in x_hist2], color='red', 
+         linestyle='--', marker='.', label='Newton\'s Method')
+
+fig1.colorbar(contourf)
+ax1.grid()
+ax1.set_ylabel("a")
+ax1.set_xlabel("tau")
+ax1.legend(loc='best')
+
+fig2, ax2 = plt.subplots()
+ax2.plot(range(len(x_hist1)), [a for a in f_hist1], color='green', linestyle='-', marker='.', label='Gradient Method')
+ax2.plot(range(len(x_hist2)), [a for a in f_hist2], color='red', linestyle='-', marker='.', label='Newton\'s Method')
+ax2.grid()
+ax2.legend(loc='best')
+
+fig3 = plt.figure()
+ax3 = fig3.add_subplot(111, projection='3d')
+ax3.plot_surface(T, A, nll_matrix)
+ax3.plot([a[0] for a in x_hist1], [a[1] for a in x_hist1], [a for a in f_hist1])    
+ax3.plot([a[0] for a in x_hist2], [a[1] for a in x_hist2], [a for a in f_hist2])
+plt.show()
+
+print('Gradient Method number of steps: {}'.format(len(x_hist1)))
+print('Gradient Method solution: {}'.format(x_hist1[-1]))
+print('Newton\'s Method number of steps: {}'.format(len(x_hist2)))
+print('Newton\'s Method solution: {}'.format(x_hist2[-1]))

min_2d.py
@@ -80,10 +80,10 @@ def grad_cds(f, x, *args):
 		
 	'''
 	grad = np.zeros((2,1)) # empty array for gradient
-	h = 10**(-3)
+	h = 10**(-7)
 	x = (x[0,0], x[1,0]) # converts input np.array to a tuple to simplify code
-	dfdx = (f(x[0] + h, x[1], *args) - f(x[0] - h, x[1], *args))/(2 * h)
-	dfdy = (f(x[0], x[1] + h, *args) - f(x[0], x[1] - h, *args))/(2 * h)
+	dfdx = (f(x[0] + h, x[1], *args) - f(x[0] - h, x[1], *args)) / (2 * h)
+	dfdy = (f(x[0], x[1] + h, *args) - f(x[0], x[1] - h, *args)) / (2 * h)
 	
 	grad[0,0] = dfdx
 	grad[1,0] = dfdy
@@ -133,20 +133,28 @@ def grad_min(f, x, a, *args):
     '''
     
     x_hist = []
+    f_hist = []
     x = np.asarray(x).reshape((2,1)) # converts tuple to array so works with helper functions
     grad = grad_cds(f, x, *args) # calculate gradient
-    x_update = x-a*grad # calcuate x_(n+1)
+    print('x: ', x, 'a: ', a, 'grad: ', grad)
+    x_update = x - a * grad # calcuate x_(n+1)
+    print('x_update: ', x_update)
     x_hist.append((x[0,0], x[1,0])) # converts back to tuple for more readable form
-    #print((x[0,0], x[1,0]))
+    #print((x[0,0], x[1,0]))    
+    f_hist.append(f(x[0,0], x[1,0], *args))
     x_hist.append((x_update[0,0], x_update[1,0]))
+    f_hist.append(f(x_update[0,0], x_update[1,0], *args))
     #print((x_update[0,0], x_update[1,0]))
     conv = convergence(f, x, x_update, *args) # determine convergence criteria
     x = x_update
 	
-    while (conv > 10**(-5)):
+    while (conv > 10**(-6)):
         grad = grad_cds(f, x, *args)
-        x_update = x-a*grad
+        print('x: ', x, 'a: ', a, 'grad: ', grad)
+        x_update = x - a * grad
+        print('x_update: ', x_update)
         x_hist.append((x_update[0,0], x[1,0])) #convert back to tuple for more readable form
+        f_hist.append(f(x_update[0,0], x_update[1,0], *args))
         #print((x_update[0,0], x_update[1,0]))
         conv = convergence(f, x, x_update, *args) # determine convergence criteria
         x = x_update
@@ -154,7 +162,7 @@ def grad_min(f, x, a, *args):
     x_min = x_hist[-1]
     f_min = f(x_min[0], x_min[1], *args)
 	
-    return x_hist, x_min, f_min
+    return x_hist, f_hist, x_min, f_min
 
 def newton_min(f, x, *args):
     '''
@@ -179,7 +187,7 @@ def newton_min(f, x, *args):
     inv_hess = lu(hess, np.identity(2))[2]
     x_hist.append((x[0,0], x[1,0]))
     f_hist.append(f(x[0,0], x[1,0], *args))
-    d = dot(inv_hess,grad)
+    d = dot(inv_hess, grad)
     x_update = x - d
     print(x_update)
     x_hist.append((x_update[0,0], x_update[1,0]))
@@ -187,7 +195,7 @@ def newton_min(f, x, *args):
     conv = convergence(f, x, x_update, *args) # determine convergence criteria
     x = x_update
 	
-    while (conv > 10**(-5)):
+    while (conv > 10**(-6)):
         grad = grad_cds(f, x, *args)
         hess = hessian(f, x, *args)
         inv_hess = lu(hess, np.identity(2))[2]
@@ -208,7 +216,7 @@ def newton_min(f, x, *args):
 def test(x, y, a, b):
 	
 	#return x**2 + y**2
-	#return np.sin(x) + np.sin(y)
+	#return np.sin(x) + np.cos(y)
 	return a * (np.cosh(b * x) + np.cosh(b * y))
 
 
@@ -252,4 +260,6 @@ if __name__ == "__main__":
 	print('Gradient Method number of steps: {}'.format(len(x_hist1)))
 	print('Newton\'s Method number of steps: {}'.format(len(x_hist2)))
 
-   # print(hessian(test, (1,1)))
+#   x = (10,10)
+#   x = np.asarray(x).reshape((2,1))
+#   print(grad_cds(test, x))